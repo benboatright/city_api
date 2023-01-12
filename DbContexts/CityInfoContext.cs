5da79016b8a04bbb9b10822d87d65c191859b452
Ben <benboatright@gmail.com>
2023-01-12 07:28:10 -0500

    sqlite connection and migrations

---
+++ DbContexts/CityInfoContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using CityInfo.API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CityInfo.API.DbContexts
+{
+	public class CityInfoContext : DbContext
+	{
+		public DbSet<City> Cities { get; set; } = null!;
+
+		public DbSet<PointOfInterest> PointsOfInterest { get; set; } = null!;
+
+		public CityInfoContext(DbContextOptions<CityInfoContext> options)
+			:base(options)
+		{
+
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<City>().HasData(
+				new City("New York City")
+				{
+					Id = 1,
+					Description = "The one with a big park."
+				},
+				new City("Antwerp")
+				{
+					Id = 2,
+					Description = "The one with a cathedral."
+				},
+				new City("Paris")
+				{
+					Id = 3,
+					Description = "The one with a tower."
+				});
+
+            modelBuilder.Entity<PointOfInterest>().HasData(
+                new PointOfInterest("Central Park")
+                {
+                    Id = 1,
+					CityId = 1,
+                    Description = "The big park."
+                },
+                new PointOfInterest("Empire State building")
+                {
+                    Id = 2,
+					CityId = 1,
+                    Description = "The building."
+                },
+                new PointOfInterest("Cathedral")
+                {
+                    Id = 3,
+					CityId = 2,
+                    Description = "The cathedral."
+                },
+                new PointOfInterest("Eiffel Tower")
+                {
+                    Id = 4,
+                    CityId = 3,
+                    Description = "The tower."
+                });
+            base.OnModelCreating(modelBuilder);
+		}
+
+		//protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		//{
+		//	optionsBuilder.UseSqlite("connecitonstring");
+		//	base.OnConfiguring(optionsBuilder);
+		//}
+	}
+}
+
---
+++ Migrations/20230112122715_Dataseed.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CityInfo.API.Migrations
+{
+    public partial class Dataseed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 1, "The one with a big park.", "New York City" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 2, "The one with a cathedral.", "Antwerp" });
+
+            migrationBuilder.InsertData(
+                table: "Cities",
+                columns: new[] { "Id", "Description", "Name" },
+                values: new object[] { 3, "The one with a tower.", "Paris" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 1, 1, "The big park.", "Central Park" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 2, 1, "The building.", "Empire State building" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 3, 2, "The cathedral.", "Cathedral" });
+
+            migrationBuilder.InsertData(
+                table: "PointsOfInterest",
+                columns: new[] { "Id", "CityId", "Description", "Name" },
+                values: new object[] { 4, 3, "The tower.", "Eiffel Tower" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "PointsOfInterest",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Cities",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
